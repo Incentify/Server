e6d0b552a6442724b6e623f6bdf5c31d186c52ba
wishinghand <wishinghand@gmail.com>
2016-10-03 14:02:43 -0700

    initial commit, using example database names

---
+++ config.js
@@ -0,0 +1,13 @@
+// what's this for?
+
+module.exports = {
+    rethinkdb: {
+        host: "db.incentify.life",
+        port: 28015,
+        authKey: "",
+        db: "rethinkdb_ex"
+    },
+    koa: {
+        port: 80
+    }
+}
